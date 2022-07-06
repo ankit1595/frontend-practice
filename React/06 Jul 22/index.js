d92fe80a395df131a6b2965c6b53896fece223fa
Ankit <ankit.95sharma@gmail.com>
2022-07-07 04:58:00 +0530

    React Components

--- React/06 Jul 22/index.js
+++ React/06 Jul 22/index.js
@@ -1,9 +1,17 @@
-/* Surprise! You probably thought you could just forget
-the line of code you just learned! Nope, not on my watch!
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import MainContent from "./MainContent";
 
-Try to write that 1-liner of React code again! This time,
-see if you can figure out how to render an <ul> with 2+ <li>s inside*/
-ReactDOM.render(
-    <ul><li>Thing 1</li><li>Thing 2</li></ul>,
-    document.getElementById("root")
-)
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
