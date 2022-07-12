355f55f71ec7f170b5707c894a709526678c0e32
Ankit <ankit.95sharma@gmail.com>
2022-07-12 11:38:06 +0530

    add more travel details

--- React/travel-journal/src/data.js
+++ React/travel-journal/src/data.js
@@ -1,6 +1,18 @@
-import shimlaP from "./images/Shimla landscape.jpeg";
+import shimla from "./images/Shimla landscape.jpeg";
+import nashik from "./images/nashik2.jpeg";
+import goa from "./images/goa.jpeg";
 
 export default [
+  {
+    title: "Trimbakeshwar Temple",
+    location: "Nashik, India",
+    googleMapsUrl: "https://goo.gl/maps/zeLuPSnqew6cGkMB6",
+    startDate: "30 May, 2022",
+    endDate: "03 Jun, 2022",
+    description:
+      "Trimbakeshwar is abode of one of the twelve Jyotirlingas. The extraordinary feature of Linga in the temple is in the form of a three faced embodying Tridev, Lord Brahma, Lord Vishnu and Lord Shiva.",
+    imageUrl: nashik,
+  },
   {
     title: "Narkanda Hills",
     location: "Shimla, India",
@@ -9,26 +21,17 @@ export default [
     endDate: "21 Mar, 2022",
     description:
       "Narkanda, a small and tranquil town located near Shimla. Encompassed by majestic hills and tropical forests, Narkanda is a hidden gem in Dev Bhoomi.",
-    imageUrl: shimlaP,
+    imageUrl: shimla,
   },
+
   {
-    title: "Mount Fuji",
-    location: "Japan",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
+    title: "Calangute Beach",
+    location: "Goa, India",
+    googleMapsUrl: "https://goo.gl/maps/mGYSPJ1PBbAXphof6",
+    startDate: "30 Dec, 2021",
+    endDate: "01 Jan, 2022",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
-  },
-  {
-    title: "Mount Fuji",
-    location: "Japan",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
-    description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+      "Calangute beach in Goa is also known as the Queen of Beaches. The period from November to February is the most pleasant to visit here. It's 15km from the capital city Panjim.",
+    imageUrl: goa,
   },
 ];
